b7de9199357877c5fbbd25c55f37b2141a97d69a
Jacob Arrington <Jacobsarrington@gmail.com>
2023-08-30 13:26:03 -0400

    fixed post file upload modal

--- react-app/src/components/PostFileModal/index.js
+++ react-app/src/components/PostFileModal/index.js
@@ -1,49 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchFile, postFile } from '../../store/dataFiles';
 
 import { useModal } from '../../context/Modal';
 import './postfile.css'
 
-
 function PostFileModal() {
   const dispatch = useDispatch()
   const { closeModal } = useModal()
+  const fileInputRef = useRef(null);
 
   const [filename, setFileName] = useState('')
   const [filetype, setFileType] = useState('')
   const [filepath, setFilepath] = useState(null);
   const [isPublic, setIsPublic] = useState(false);
-
   const [errors, setErrors] = useState([])
+
+  const triggerFileInput = () => {
+    fileInputRef.current.click(); 
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
     const formData = new FormData();
     formData.append('file', filepath);
     formData.append('filename', filename)
     formData.append('filetype', filetype)
 
-
-
-
     try {
       const data = await dispatch(postFile(formData));
       await dispatch(fetchFile());
-  
       if (data.error) {
-        // Handle the error case
         setErrors([data.error]);
       } else {
-        // Handle the success case
         closeModal();
       }
     } catch (err) {
-      // Handle unexpected errors, e.g., network issues or server crashes
       closeModal()
     }
-   
   }
 
   const handleFileUpload = (e) => {
@@ -57,9 +51,10 @@ function PostFileModal() {
 
     setFileName(cleanedName);
     setFileType(file.type);
-}
+  }
+
   return (
-    <div className="modal-content-container"> {/* Outer Container */}
+    <div className="modal-content-container"> 
       <h1>Add File</h1>
       <form onSubmit={handleSubmit} className="add-file-form" encType='multipart/form-data'>
         <ul>
@@ -67,39 +62,26 @@ function PostFileModal() {
             <li key={idx}>{error}</li>
           ))}
         </ul>
-        <div className="input-container"> {/* Input Container */}
-          {/* <label>
-            File Name
-            <input
-              type="text"
-              value={filename}
-              onChange={(e) => setFileName(e.target.value)}
-              required
-              className="add-file-input"
-            />
-          </label> */}
+        <div className="input-container"> 
           <label className="custom-file-upload">
-            <input type="file" style={{ display: "none" }} onChange={handleFileUpload} required />
-            </label>
-            {filepath ? filepath.name : "No file chosen yet..."}
-            <div className="upload-btn">Upload File</div>
-          
-          
+            <input ref={fileInputRef} type="file" style={{ display: "none" }} onChange={handleFileUpload} required />
+          </label>
+          {filepath ? filepath.name : "No file chosen yet..."}
+          <div className="upload-btn" onClick={triggerFileInput}>Upload File</div>
           <label>
             Public File
-            </label>
-            <input
-              type="checkbox"
-              checked={isPublic}
-              onChange={(e) => setIsPublic(e.target.checked)}
-              className="public-file-checkbox"
-            />
-         
-        
+          </label>
+          <input
+            type="checkbox"
+            checked={isPublic}
+            onChange={(e) => setIsPublic(e.target.checked)}
+            className="public-file-checkbox"
+          />
         </div>
         <button type="submit" className="add-file-button">Add File</button>
       </form>
     </div>
   );
 }
+
 export default PostFileModal;
